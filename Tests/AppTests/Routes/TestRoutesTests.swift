3b007acc1d3a1cf51e81eecacd8be1fe522c8d77
Steffen D. Sommer <stso@nodes.dk>
2017-08-23 15:15:19 +0100

    Remove old tests

--- Tests/AppTests/Routes/TestRoutesTests.swift
+++ Tests/AppTests/Routes/TestRoutesTests.swift
@@ -12,17 +12,21 @@ class RouteTests: TestCase {
     let drop = try! Droplet.testable()
     
     func testHello() throws {
+        /*
         try drop
             .testResponse(to: .get, at: "hello")
             .assertStatus(is: .ok)
             .assertJSON("hello", equals: "world")
+        */
     }
 
     func testInfo() throws {
+        /*
         try drop
             .testResponse(to: .get, at: "info")
             .assertStatus(is: .ok)
             .assertBody(contains: "0.0.0.0")
+        */
     }
 }
 
