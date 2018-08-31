16f9b90c8f3adc5f79b5e1acbe0882827ffdd5bd
Brett R. Toomey <brto@nodes.dk>
2018-08-31 11:11:42 +0200

    Updated deps

--- Package.swift
+++ Package.swift
@@ -2,16 +2,39 @@
 import PackageDescription
 
 let package = Package(
-    name: "VaporApp",
+    name: "NodesTemplate",
     dependencies: [
-        // 💧 A server-side Swift web framework.
+        // Vapor
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/fluent-mysql.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/redis.git", from: "3.0.0"),
 
-        // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
-        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0")
+        // Nodes
+        .package(url: "https://github.com/nodes-vapor/jwt-keychain.git", from: "1.0.0-beta"),
+        .package(url: "https://github.com/nodes-vapor/sugar.git", from: "3.0.0-beta"),
+        .package(url: "https://github.com/nodes-vapor/n-meta.git", from: "3.0.0-beta"),
+        .package(url: "https://github.com/nodes-vapor/admin-panel.git", .upToNextMinor(from:"2.0.0-beta")),
+        .package(url: "https://github.com/nodes-vapor/submissions.git", from: "1.0.0-beta"),
+        .package(url: "https://github.com/nodes-vapor/nodes-sso.git", from: "1.0.0-beta"),
+        .package(url: "https://github.com/nodes-vapor/paginator.git", .branch("vapor-3")),
+
+        // 3rd Party
+
     ],
     targets: [
-        .target(name: "App", dependencies: ["FluentSQLite", "Vapor"]),
+        .target(name: "App", dependencies: [
+            "Vapor",
+            "FluentMySQL",
+            "Redis",
+            "JWTKeychain",
+            "Sugar",
+            "NMeta",
+            "AdminPanel",
+            "Submissions",
+            "Mailgun",
+            "NodesSSO",
+            "Paginator"
+        ]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
