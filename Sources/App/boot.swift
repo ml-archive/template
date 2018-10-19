4e9289391a4e285c3508439fa2b7f1c4d3bdda37
Steffen D. Sommer <steffendsommer@icloud.com>
2018-10-19 14:40:13 +0200

    Add leaf tag example

--- Sources/App/boot.swift
+++ Sources/App/boot.swift
@@ -1,6 +1,12 @@
+import Sugar
 import Vapor
 
 /// Called after your application has initialized.
 public func boot(_ app: Application) throws {
-    // your code here
+    // MARK: Leaf tags
+    // Add your tags like this:
+    // let tags = try app.make(MutableLeafTagConfig.self)
+    // tags.use(
+    //     MyLeafTag()
+    // )
 }
