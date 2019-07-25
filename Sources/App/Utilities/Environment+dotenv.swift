38151f2bfef7eeff4478b815219d9d203a415838
Rasmus Ebbesen <re@nodes.dk>
2019-07-25 09:13:54 +0200

    Update Sources/App/Utilities/Environment+dotenv.swift
    Co-Authored-By: Siemen Sikkema <siemensikkema@users.noreply.github.com>

--- Sources/App/Utilities/Environment+dotenv.swift
+++ Sources/App/Utilities/Environment+dotenv.swift
@@ -67,7 +67,7 @@ public extension Environment {
             // remove surrounding single quotes from value
             // & convert remove escape character before any embedded quotes
             if
-                value[value.startIndex] == "'" &&
+                value[value.startIndex] == "'",
                 value[value.index(before: value.endIndex)] == "'"
             {
                 value.remove(at: value.startIndex)
